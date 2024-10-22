96f8afb61c5ccc4f66a76c01f814bcacbd35ac18
mamun-prodhan <mamunpr9@gmail.com>
2024-10-22 23:23:37 +0600

    lucky array implemented

---
+++ lucky.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    int a[n];
+
+    for(int i = 0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    int min = a[0];
+
+    for(int i=0; i<n; i++){
+        if(min>a[i]){
+            min=a[i];
+        }
+    }
+    int frequency = 0;
+
+    for(int i=0; i<n; i++){
+        if(a[i] == min){
+            frequency +=1;
+        }
+    }
+
+    if(frequency % 2 != 0){
+        printf("Lucky");
+    }
+    else{
+        printf("Unlucky");
+    }
+
+    return 0;
+}
